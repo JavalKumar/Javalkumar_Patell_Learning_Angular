799e32f499d6a252670dbb4cf467d1e700abce94
“Javalkumar <W0845066@myscc.ca>
2024-09-24 14:45:47 -0400

    A3-Step8

--- JavalPatelLearningAngular/src/app/cars-list/cars-list.component.ts
+++ JavalPatelLearningAngular/src/app/cars-list/cars-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { User } from '../Shared/Modules/user';
 
 @Component({
   selector: 'app-cars-list',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './cars-list.component.html',
   styleUrl: './cars-list.component.css'
 })
 export class CarsListComponent {
-
+  @input() carList?:User;
 }
--- JavalPatelLearningAngular/src/app/cars/cars.component.ts
+++ JavalPatelLearningAngular/src/app/cars/cars.component.ts
@@ -1,12 +1,23 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
+import { User } from '../Shared/Modules/user';
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
+  tableColumn : string[]=['Id', 'Company', 'Modle', 'Price', 'isPertol'];
 
+  car1 : User = {id: 1, companyName: "Lamborghini", model: "Huracane",price: 200000, isPetrol: true }
+  car2 : User = {id: 2, companyName: "Lamborghini", model: "Avantador",price: 235000, isPetrol: true }
+  car3 : User = {id: 3, companyName: "Lamborghini", model: "Gallardo",price: 155000, isPetrol: false }
+  car4 : User = {id: 4, companyName: "Lamborghini", model: "Diablo",price: 120000, isPetrol: true }
+  car5 : User = {id: 5, companyName: "Lamborghini", model: "Urus",price: 190000, isPetrol: false }
+  car6 : User = {id: 6, companyName: "Lamborghini", model: "Countach",price: 1200000, isPetrol: true }
+
+  carList: User[] = [this.car1,this.car2,this.car3,this.car4,this.car5,this.car6]
 }
