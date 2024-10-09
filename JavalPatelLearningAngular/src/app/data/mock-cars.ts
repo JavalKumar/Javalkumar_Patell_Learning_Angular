728c5b1e1c1fab62453738b672b3dcb4932732da
JavalKumar <w0845066@myscc.ca>
2024-10-09 17:39:51 -0400

    updates

--- JavalPatelLearningAngular/src/app/data/mock-cars.ts
+++ JavalPatelLearningAngular/src/app/data/mock-cars.ts
@@ -1,4 +1,5 @@
-import {user} from '../Shared/Modules/user';
+// import {user} from '../Shared/Modules/user';
+import{User} from '../Shared/Modules/user';
 
 export const carList:User[] = [
     {id: 1, companyName: "Lamborghini", model: "Huracane",price: 200000, isPetrol: true },
@@ -6,6 +7,5 @@ export const carList:User[] = [
     {id: 3, companyName: "Lamborghini", model: "Gallardo",price: 155000, isPetrol: false },
     {id: 4, companyName: "Lamborghini", model: "Diablo",price: 120000, isPetrol: true },
     {id: 5, companyName: "Lamborghini", model: "Urus",price: 190000, isPetrol: false },
-    {id: 6, companyName: "Lamborghini", model: "Countach",price: 1200000, isPetrol: true }
-    
+    {id: 6, companyName: "Lamborghini", model: "Countach",price: 1200000, isPetrol: true } 
 ]
