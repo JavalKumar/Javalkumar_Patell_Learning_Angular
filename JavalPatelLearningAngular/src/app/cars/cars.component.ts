21f400031be2ae0b4918c247809d2cf15a796fd1
JavalKumar <w0845066@myscc.ca>
2024-10-11 16:09:16 -0400

    update

--- JavalPatelLearningAngular/src/app/cars/cars.component.ts
+++ JavalPatelLearningAngular/src/app/cars/cars.component.ts
@@ -13,14 +13,14 @@ import { CarsListComponent } from "../cars-list/cars-list.component";
 export class CarsComponent {
   tableColumn : string[]=['Id', 'Company', 'Modle', 'Price', 'isPertol'];
 
-  // carList: User[] = [
-  //   { id: 1, companyName: "Lamborghini", model: "Huracan", price: 200000, isPetrol: true },
-  //   { id: 2, companyName: "Lamborghini", model: "Aventador", price: 235000, isPetrol: true },
-  //   { id: 3, companyName: "Lamborghini", model: "Gallardo", price: 155000, isPetrol: false },
-  //   { id: 4, companyName: "Lamborghini", model: "Diablo", price: 120000, isPetrol: true },
-  //   { id: 5, companyName: "Lamborghini", model: "Urus", price: 190000, isPetrol: false },
-  //   { id: 6, companyName: "Lamborghini", model: "Countach", price: 1200000, isPetrol: true }
-  // ];
+  carList: User[] = [
+    { id: 1, companyName: "Lamborghini", model: "Huracan", price: 200000, isPetrol: true },
+    { id: 2, companyName: "Lamborghini", model: "Aventador", price: 235000, isPetrol: true },
+    { id: 3, companyName: "Lamborghini", model: "Gallardo", price: 155000, isPetrol: false },
+    { id: 4, companyName: "Lamborghini", model: "Diablo", price: 120000, isPetrol: true },
+    { id: 5, companyName: "Lamborghini", model: "Urus", price: 190000, isPetrol: false },
+    { id: 6, companyName: "Lamborghini", model: "Countach", price: 1200000, isPetrol: true }
+  ];
 
   carSelected?: User;
 
